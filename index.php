a5cd99d94bc30b631d210471c6288a7c06913cd5
jimbursch <jim@mymindshare.com>
2014-09-21 07:51:44 -0700

    reduced limit on notices

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if( !isset($_SESSION['lastSeenHome']) || $_SESSION['lastSeenHome'] < $User->time
 
 class libHome
 {
-	static public function getType($type=false, $limit=35)
+	static public function getType($type=false, $limit=5)
 	{
 		global $DB, $User;
 
@@ -395,11 +395,28 @@ if( !$User->type['User'] )
 	<?php
 		$welcomtext='';
 		$welcomtext.='<p><b>SPECIAL NOTICE: BitDip is an open source web development project in it\'s very early stages. Jim Bursch is the lead developer and he can be found on GitHub: <a href="https://github.com/jimbursch">https://github.com/jimbursch</a>.</b></p>';
+
 		$welcomtext.='<p><b>What is Bit?</b><br />A bit is one/millionth of a <a href="https://en.wikipedia.org/wiki/Bitcoin">bitcoin</a>, which is a digital currency or money.</p>';
+
 		$welcomtext.='<p><b>What is Dip?</b><br />Dip is a game of grand strategy forked from the open source <a href="http://webdiplomacy.net">webDiplomacy</a> project.</p>';
-		$welcomtext.='<p><b>What is BitDip?</b><br />BitDip is a game of grand strategy combined with a system of finance denominated in Bitcoin. Essentially, at BitDip you play for real money.</p>';
+
+		$welcomtext.='<p><b>What is BitDip?</b><br />BitDip is a game of grand strategy combined with a system of finance denominated in Bitcoin.</p>';
+
+		$welcomtext.='<p><b>How do you play the game?</b><br />The game mechanics of BitDip are essentially the same as the game mechanics of webDiplomacy using two map variants: modern Europe and the WWIV global map.</p><p>The following are rule variants from classic webDiplomacy:</p><ul><li>Players can enter a game at the beginning by purchasing neutral supply centers. Funds from the purchase of neutral supply centers go into the game account. At the end of the game, funds are divided among surviving players, per supply center held at the time of liquidation.</li><li>Game play begins when all neutral supply centers have been purchased.</li><li>Games end when all surviving players vote to liquidate the game and divide up the game funds.</li><li>A player can enter a game at any time by purchasing some or all of the supply centers of another player. This is a transaction strictly between players and does not affect the game funds.</li><li>The only way to exit a game before the end is to sell out to another player.</li><li>There are no "countries" in BitDip. There are only players who own/occupy territories with supply centers.</li><li>The buying and selling of supply centers takes place at the end of the build phase.</li><li>All supply centers may build units</li></ul>';
+
+		$welcomtext.='<p><b>Is it possible for a game to never end?</b><br />Yes, a game does not end until all surviving players vote to liquidate. Theoretically, a game can go on forever with old players selling out and new players buying in.</p>';
+
+		$welcomtext.='<p><b>What are BitDip guilds?</b><br />Guilds are organized groups of players who help each other play the game. All players are encouraged to join a guild for assistance, advice, support, and to protect themselves from other guilds.</p>';
+
+		$welcomtext.='<p><b>Is meta-gaming allowed?</b><br />Meta-gaming occurs when players communicate outside a game to coordinate in-game actions. Since it is impossible to enforce a rule against meta-gaming, it is allowed and players are welcome to do their own meta-gaming (such as joining a guild).</p>';
+
+		$welcomtext.='<p><b>Is multi-accounting allowed?</b><br />Multi-accounting (creating and playing with more than one account) is allowed for the same reason as meta-gaming. However, we don\'t encourage the practice</p>';
+
 		$welcomtext.='<p><b>How is BitDip different from classic Diplomacy?</b><br />The classic game of <a href="https://en.wikipedia.org/wiki/Diplomacy_%28game%29">Diplomacy</a> is a board game that was invented in the 1950\'s and is currently owned by a subsidiary of Hasbro called Wizards of the Coast. Soon after the game\'s invention and commercialization, the player community developed the game into a sophisticated hobby that went far beyond the face-to-face boardgame. Diplomacy was the first game to be extensively played by mail, then by email, and now the game is played online around the world. Thanks to the dedication of the open source developers of webDiplomacy (formerly phpDiplomacy), Diplomacy the hobby (not the boardgame) can be played any time, any where, with any player who can connect to the Web.</p>';
+
 		$welcomtext.='<p>BitDip is forked from webDiplomacy but it is not the same thing as webDiplomacy and it is certainly nothing like the boardgame.</p><p>The goal in BitDip is not to "win" a game, but rather to increase wealth by controlling more supply centers and buying/selling supply centers profitably. A player enters a game by purchasing supply centers and exits a game by selling supply centers. The only way a game ends is if all the players agree to liquidate their supply centers, which is to cash-in the supply centers at par value, which was set when the game was created. Otherwise, the only way out of a game is to either be eliminated or sell out to another player.';
+
+
 		$welcomtext.='<p><b>How it works:</b></p>';
 		$welcomtext.='<p>When a game is created, a par value is set for every supply center (e.g. 100 bits). To enter the game, a player has to purchase neutral supply centers at that par value. Let\'s say player Alpha enters the game by purchasing 3 supply centers. The purchase price of those centers (300 bits) goes into the game account, which sets the liquidation value of the game. At this point if Alpha wants to get out of the game and get his money back, he has to move around the board, capture all the supply centers, and liquidate the game. But rather than do that, Alpha waits for another player, Bravo, to enter the game. Bravo purchases 3 supply centers, which increases the liquidation value of the game to 600 bits. Now, if Alpha moves around the board, captures all the supply centers (including those held by Bravo), then liquidates the game, he will make 600 bits, a nice return on his 300 bit investment.</p>';
 		$welcomtext.='<p>Let\'s say for the sake of this illustration there are 10 supply centers on the board (there are 34 in classic Diplomacy). If 10 players enter the game, then the liquidation value of the game will be 1000 bits at 100 bits per supply center. Let\'s say through the course of play, Alpha and Bravo succeed in eliminating the other players and they each now hold 5 supply centers. They have profitted handsomely from their skilled play. If they are both satisfied, they can agree to liquidate the board and cash out (500 bits each). However, let\'s say Alpha doesn\'t want to liquidate. He wants to keep playing and try to win more. They cannnot liquidate unless they both agree, so if Bravo wants out, he has to find another way to exit the game. He can do so by selling his supply centers to another player, and he can offer an incentive by aggreeing to sell his 5 centers below par. Let\'s say Charlie is willing to buy the 5 centers for 400 bits. Bravo gets out of the game profitably -- he spent 100 bits to get in, and made 300 more when he sold out. Now if Charlie continues to play with Alpha, and let\'s say nothing changes and they both decide to liquidate, Charlie will make 100 bits (500 at liqudation minus the 400 he paid Bravo) and Alpha makes 400 bits (500 at liquidation minus 100 he paid to enter the game).';
