7f2fe4ca9dccd4d15997140fee7afce8affe548c
jimbursch <jim@mymindshare.com>
2014-09-24 06:36:06 -0700

    changes

--- variants/Modern2/variant.php
+++ variants/Modern2/variant.php
@@ -18,7 +18,7 @@
 	along with webDiplomacy. If not, see <http://www.gnu.org/licenses/>.
 
 	---
-
+	
 	Changelog:
 	1.0:     initial release by Carey Jensen
 	1.5:     new webdip v.98 code by Oliver Auth
@@ -40,8 +40,8 @@ class Modern2Variant extends WDVariant {
 	public $id         = 19;
 	public $mapID      = 19;
 	public $name       = 'Modern2';
-	public $fullName   = 'Modern Europe';
-	public $description= 'Modern Europe is a modification of the Modern Diplomacy II variant.';
+	public $fullName   = 'Modern Diplomacy II';
+	public $description= 'Modern Diplomacy II is intended to be diplomacy with an updated map, circa 1994, taking place in Europe, the Middle East and North Africa.';
 	public $author     = 'Vincent Mous';
 	public $adapter    = 'Carey Jensen / Oliver Auth';
 	public $version    = 'II';
@@ -59,18 +59,22 @@ class Modern2Variant extends WDVariant {
 		$this->variantClasses['OrderInterface']     = 'Modern2';
 		$this->variantClasses['processOrderBuilds'] = 'Modern2';
 		$this->variantClasses['processOrderDiplomacy'] = 'Modern2';
+<<<<<<< HEAD
 
 		$this->variantClasses['processGame'] = 'Modern2';
 
 
 
+=======
+		
+>>>>>>> parent of 1848f49... Add CustomStart to Modern2
 	}
 
 	public function initialize() {
 		parent::initialize();
 		$this->supplyCenterTarget = 33;
 	}
-
+	
 	public function turnAsDate($turn) {
 		if ( $turn==-1 ) return "Pre-game";
 		else return ( $turn % 2 ? "Autumn, " : "Spring, " ).(floor($turn/2) + 1994);
@@ -82,9 +86,6 @@ class Modern2Variant extends WDVariant {
 			else return ( turn%2 ? "Autumn, " : "Spring, " )+(Math.floor(turn/2) + 1994);
 		};';
 	}
-
-
-
 }
 
 ?>
